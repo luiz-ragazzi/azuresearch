8dd11c056b3b33c8bc1c1e6b08082a169a4b0933
Luiz Ragazzi <lr@labee.co.uk>
2015-07-24 17:15:56 +0100

    First Commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Azure.Search.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UhyIntranet.Common;
+
+namespace SimpleSearchMVCApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        private FeaturesSearch _featuresSearch = new FeaturesSearch("");
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Search(string q = "", string f = "", string[] channels = null)
+        {   
+            // If blank search, assume they want to search everything
+            if (string.IsNullOrWhiteSpace(q))
+                q = "*";
+
+
+
+            return new JsonResult
+            {
+                JsonRequestBehavior = JsonRequestBehavior.AllowGet,
+                
+                Data = _featuresSearch.Search(q, new ODataFilter(new IntranetNewsFilter(f, channels)))
+            };
+        }
+
+       
+
+
+    }
+}
---
+++ FeaturesSearch.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Azure.Search;
+using Microsoft.Azure.Search.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Web;
+using UhyIntranet.Common;
+
+namespace SimpleSearchMVCApp
+{
+    public class FeaturesSearch
+    {
+        private static SearchServiceClient _searchClient;
+        private static SearchIndexClient _indexClient;
+
+        public static string errorMessage;
+
+        public FeaturesSearch(string s)
+        {
+            
+        }
+
+        static FeaturesSearch()
+        {
+            try
+            {
+                string searchServiceName = ConfigurationManager.AppSettings["SearchServiceName"];
+                string apiKey = ConfigurationManager.AppSettings["SearchServiceApiKey"];
+
+
+                // Create an HTTP reference to the catalog index
+                _searchClient = new SearchServiceClient(searchServiceName, new SearchCredentials(apiKey));
+                _indexClient = _searchClient.Indexes.GetClient("intranetnews");
+            }
+            catch (Exception e)
+            {
+                errorMessage = e.Message;
+            }
+        }
+
+
+
+        public DocumentSearchResponse Search(string searchText, ODataFilter filter)
+        {
+            // Execute search based on query string
+            try
+            {   
+                var sp = new SearchParameters() { SearchMode = SearchMode.All };
+                sp.Filter = filter.GetFilter();
+                return _indexClient.Documents.Search(searchText, sp);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error querying index: {0}\r\n", ex.Message);
+            }
+            return null;
+        }
+
+    }
+}
